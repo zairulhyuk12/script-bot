df0394f2cc292e9035dba44a965a4e5436dd2c34
zairulhyuk12 <51875854+zairulhyuk12@users.noreply.github.com>
2022-07-28 02:18:46 +0800

    Add files via upload

---
+++ dll/system/bckp.sh
@@ -0,0 +1,170 @@
+#!/bin/bash
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#########################
+
+BURIQ () {
+    curl -sS https://raw.githubusercontent.com/syfqsamvpn/access/main/ip > /root/tmp
+    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
+    for user in "${data[@]}"
+    do
+    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
+    d1=(`date -d "$exp" +%s`)
+    d2=(`date -d "$biji" +%s`)
+    exp2=$(( (d1 - d2) / 86400 ))
+    if [[ "$exp2" -le "0" ]]; then
+    echo $user > /etc/.$user.ini
+    else
+    rm -f /etc/.$user.ini > /dev/null 2>&1
+    fi
+    done
+    rm -f /root/tmp
+}
+
+MYIP=$(curl -sS ipv4.icanhazip.com)
+Name=$(curl -sS https://raw.githubusercontent.com/syfqsamvpn/access/main/ip | grep $MYIP | awk '{print $2}')
+echo $Name > /usr/local/etc/.$Name.ini
+CekOne=$(cat /usr/local/etc/.$Name.ini)
+
+Bloman () {
+if [ -f "/etc/.$Name.ini" ]; then
+CekTwo=$(cat /etc/.$Name.ini)
+    if [ "$CekOne" = "$CekTwo" ]; then
+        res="Expired"
+    fi
+else
+res="Permission Accepted..."
+fi
+}
+
+PERMISSION () {
+    MYIP=$(curl -sS ipv4.icanhazip.com)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/syfqsamvpn/access/main/ip | awk '{print $4}' | grep $MYIP)
+    if [ "$MYIP" = "$IZIN" ]; then
+    Bloman
+    else
+    res="Permission Denied!"
+    fi
+    BURIQ
+}
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+PERMISSION
+if [ -f /home/needupdate ]; then
+red "Your script need to update first !"
+exit 0
+elif [ "$res" = "Permission Accepted..." ]; then
+echo -ne
+else
+red "Permission Denied!"
+exit 0
+fi
+
+IP=$(curl -sS ipv4.icanhazip.com);
+date=$(date +"%Y-%m-%d")
+
+MYIP=$(curl -sS ipv4.icanhazip.com)
+NameUser=$(curl -sS https://raw.githubusercontent.com/syfqsamvpn/access/main/ip | grep $MYIP | awk '{print $2}')
+
+
+clear
+echo -e "[ ${green}INFO${NC} ] Create password for database"
+read -rp "Enter password : " -e InputPass
+sleep 1
+if [[ -z $InputPass ]]; then
+exit 0
+fi
+echo -e "[ ${green}INFO${NC} ] Processing... "
+mkdir -p /root/backup
+sleep 1
+
+cp /etc/passwd backup/
+cp /etc/group backup/
+cp /etc/shadow backup/
+cp /etc/gshadow backup/
+cp -r /etc/wireguard backup/wireguard
+cp /etc/ppp/chap-secrets backup/chap-secrets
+cp /etc/ipsec.d/passwd backup/passwd1
+cp /etc/shadowsocks-libev/akun.conf backup/ss.conf
+cp -r /var/lib/premium-script/ backup/premium-script
+cp -r /home/sstp backup/sstp
+cp -r /etc/v2ray backup/v2ray
+cp -r /etc/trojan backup/trojan
+cp -r /usr/local/shadowsocksr/ backup/shadowsocksr
+cp -r /home/vps/public_html backup/public_html
+cp -r /etc/xray-mini backup/xray-mini
+cd /root
+zip -rP $InputPass $NameUser.zip backup > /dev/null 2>&1
+
+##############++++++++++++++++++++++++#############
+LLatest=`date`
+Get_Data () {
+git clone https://github.com/syfqsamvpn/user-backup-db.git /root/user-backup/ &> /dev/null
+}
+
+Mkdir_Data () {
+mkdir -p /root/user-backup/$NameUser
+}
+
+Input_Data_Append () {
+if [ ! -f "/root/user-backup/$NameUser/$NameUser-last-backup" ]; then
+touch /root/user-backup/$NameUser/$NameUser-last-backup
+fi
+echo -e "User         : $NameUser
+last-backup : $LLatest
+" >> /root/user-backup/$NameUser/$NameUser-last-backup
+mv /root/$NameUser.zip /root/user-backup/$NameUser/
+}
+
+Save_And_Exit () {
+    cd /root/user-backup
+    git config --global user.email "syfqpubg5@gmail.com" &> /dev/null
+    git config --global user.name "syfqsamvpn" &> /dev/null
+    rm -rf .git &> /dev/null
+    git init &> /dev/null
+    git add . &> /dev/null
+    git commit -m m &> /dev/null
+    git branch -M main &> /dev/null
+    git remote add origin https://github.com/syfqsamvpn/user-backup-db
+    git push -f https://ghp_tokensendiri@github.com/syfqsamvpn/user-backup-db.git &> /dev/null
+}
+
+if [ ! -d "/root/user-backup/" ]; then
+sleep 1
+echo -e "[ ${green}INFO${NC} ] Getting database... "
+Get_Data
+Mkdir_Data
+sleep 1
+echo -e "[ ${green}INFO${NC} ] Getting info server... "
+Input_Data_Append
+sleep 1
+echo -e "[ ${green}INFO${NC} ] Processing updating server...... "
+Save_And_Exit
+fi
+link="https://raw.githubusercontent.com/syfqsamvpn/user-backup-db/main/$NameUser/$NameUser.zip"
+sleep 1
+echo -e "[ ${green}INFO${NC} ] Backup done "
+sleep 1
+echo
+sleep 1
+echo -e "[ ${green}INFO${NC} ] Generete Link Backup "
+echo
+sleep 2
+echo -e "The following is a link to your vps data backup file.
+Your VPS IP $IP
+
+$link
+save the link pliss!
+
+If you want to restore data, please enter the link above.
+Thank You For Using Our Services"
+
+rm -rf /root/backup &> /dev/null
+rm -rf /root/user-backup &> /dev/null
+rm -f /root/$NameUser.zip &> /dev/null
+echo
+read -n 1 -s -r -p "Press any key to back on menu"
+system-menu
---
+++ menu_all/setting-menu.sh
@@ -0,0 +1,316 @@
+#!/bin/bash
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#########################
+
+BURIQ () {
+    curl -sS https://raw.githubusercontent.com/syfqsamvpn/access/main/ip > /root/tmp
+    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
+    for user in "${data[@]}"
+    do
+    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
+    d1=(`date -d "$exp" +%s`)
+    d2=(`date -d "$biji" +%s`)
+    exp2=$(( (d1 - d2) / 86400 ))
+    if [[ "$exp2" -le "0" ]]; then
+    echo $user > /etc/.$user.ini
+    else
+    rm -f /etc/.$user.ini > /dev/null 2>&1
+    fi
+    done
+    rm -f /root/tmp
+}
+
+MYIP=$(curl -sS ipv4.icanhazip.com)
+Name=$(curl -sS https://raw.githubusercontent.com/syfqsamvpn/access/main/ip | grep $MYIP | awk '{print $2}')
+echo $Name > /usr/local/etc/.$Name.ini
+CekOne=$(cat /usr/local/etc/.$Name.ini)
+
+Bloman () {
+if [ -f "/etc/.$Name.ini" ]; then
+CekTwo=$(cat /etc/.$Name.ini)
+    if [ "$CekOne" = "$CekTwo" ]; then
+        res="Expired"
+    fi
+else
+res="Permission Accepted..."
+fi
+}
+
+PERMISSION () {
+    MYIP=$(curl -sS ipv4.icanhazip.com)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/syfqsamvpn/access/main/ip | awk '{print $4}' | grep $MYIP)
+    if [ "$MYIP" = "$IZIN" ]; then
+    Bloman
+    else
+    res="Permission Denied!"
+    fi
+    BURIQ
+}
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+PERMISSION
+
+if [ -f /home/needupdate ]; then
+red "Your script need to update first !"
+exit 0
+elif [ "$res" = "Permission Accepted..." ]; then
+echo -ne
+else
+red "Permission Denied!"
+exit 0
+fi
+clear
+
+x="ok"
+
+cekray=`cat /root/log-install.txt | grep -ow "XRAY" | sort | uniq`
+if [ "$cekray" = "XRAY" ]; then
+kjj='xray'
+else
+kjj='v2ray'
+fi
+[[ -f /etc/ontorrent ]] && sts="\033[0;32mON \033[0m" || sts="\033[1;31mOFF\033[0m"
+
+enabletorrent() {
+[[ ! -f /etc/ontorrent ]] && {
+sudo iptables -A FORWARD -m string --string "get_peers" --algo bm -j DROP
+sudo iptables -A FORWARD -m string --string "announce_peer" --algo bm -j DROP
+sudo iptables -A FORWARD -m string --string "find_node" --algo bm -j DROP
+sudo iptables -A FORWARD -m string --algo bm --string "BitTorrent" -j DROP
+sudo iptables -A FORWARD -m string --algo bm --string "BitTorrent protocol" -j DROP
+sudo iptables -A FORWARD -m string --algo bm --string "peer_id=" -j DROP
+sudo iptables -A FORWARD -m string --algo bm --string ".torrent" -j DROP
+sudo iptables -A FORWARD -m string --algo bm --string "announce.php?passkey=" -j DROP
+sudo iptables -A FORWARD -m string --algo bm --string "torrent" -j DROP
+sudo iptables -A FORWARD -m string --algo bm --string "announce" -j DROP
+sudo iptables -A FORWARD -m string --algo bm --string "info_hash" -j DROP
+sudo iptables-save > /etc/iptables.up.rules
+sudo iptables-restore -t < /etc/iptables.up.rules
+sudo netfilter-persistent save >/dev/null 2>&1
+sudo netfilter-persistent reload >/dev/null 2>&1
+touch /etc/ontorrent
+setting-menu
+} || {
+sudo iptables -D FORWARD -m string --string "get_peers" --algo bm -j DROP
+sudo iptables -D FORWARD -m string --string "announce_peer" --algo bm -j DROP
+sudo iptables -D FORWARD -m string --string "find_node" --algo bm -j DROP
+sudo iptables -D FORWARD -m string --algo bm --string "BitTorrent" -j DROP
+sudo iptables -D FORWARD -m string --algo bm --string "BitTorrent protocol" -j DROP
+sudo iptables -D FORWARD -m string --algo bm --string "peer_id=" -j DROP
+sudo iptables -D FORWARD -m string --algo bm --string ".torrent" -j DROP
+sudo iptables -D FORWARD -m string --algo bm --string "announce.php?passkey=" -j DROP
+sudo iptables -D FORWARD -m string --algo bm --string "torrent" -j DROP
+sudo iptables -D FORWARD -m string --algo bm --string "announce" -j DROP
+sudo iptables -D FORWARD -m string --algo bm --string "info_hash" -j DROP
+sudo iptables-save > /etc/iptables.up.rules
+sudo iptables-restore -t < /etc/iptables.up.rules
+sudo netfilter-persistent save >/dev/null 2>&1
+sudo netfilter-persistent reload >/dev/null 2>&1
+rm -f /etc/ontorrent
+setting-menu
+}
+}
+
+while true $x != "ok"
+do
+
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\E[44;1;39m                ⇱ SETTINGS  MENU ⇲                \E[0m"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\033[1;33mSettings\033[0m :
+
+[\033[0;32m01\033[0m] • Add/Change VPS Host Subdomain
+[\033[0;32m02\033[0m] • Add Subdomain to Cloudflare
+[\033[0;32m03\033[0m] • Add Pointing Bug
+[\033[0;32m04\033[0m] • Renew Certificate $kjj
+[\033[0;32m05\033[0m] • Change Banner VPS
+[\033[0;32m06\033[0m] • Change VPS Auto Reboot Time
+[\033[0;32m07\033[0m] • Limit Bandwidth Speed Server
+[\033[0;32m100\033[0m] • Restart All Service
+[\033[0;32m101\033[0m] • Disable Torrent $sts
+[\033[0;32m102\033[0m] • Ram Cache Cleaner
+[\033[0;32m111\033[0m] • SETUP TELCO BUG
+
+\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\033[1;33mChange-Port\033[0m :
+
+[\033[0;32m08\033[0m] • Change Port Stunnel4
+[\033[0;32m09\033[0m] • Change Port OpenVPN
+[\033[0;32m10\033[0m] • Change Port Wireguard
+[\033[0;32m11\033[0m] • Change Port Vmess
+[\033[0;32m12\033[0m] • Change Port Trojan
+[\033[0;32m13\033[0m] • Change Port Trojan-GO
+[\033[0;32m14\033[0m] • Change Port Squid
+[\033[0;32m15\033[0m] • Change Port SSTP
+[\033[0;32m16\033[0m] • Change Port SHDO WS
+[\033[0;32m17\033[0m] • Change Port SHSSL WS
+[\033[0;32m18\033[0m] • Change Port OHP
+[\033[0;32m19\033[0m] • Change Port VlessXTLs
+[\033[0;32m20\033[0m] • Change Port Vless"
+if [ "$cekray" = "XRAY" ]; then
+echo -e "[\033[0;32m22\033[0m] • Change Port VmessGrpc
+[\033[0;32m23\033[0m] • Change Port VlessGrpc
+[\033[0;32m24\033[0m] • Change Port TrojanGrpc"
+echo -e ""
+else
+echo -e "[\033[0;32m21\033[0m] • Change Port VmessGrpc"
+echo ""
+fi
+echo -e "[00] • Back to Main Menu \033[1;32m<\033[1;33m<\033[1;31m<\033[1;31m"
+echo ""
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+echo -ne "Select menu : "; read x
+
+case "$x" in 
+   1 | 01)
+   clear
+   add-host
+   break
+   ;;
+   2 | 02)
+   clear
+   cloudflare-setting
+   break
+   ;;
+   3 | 03)
+   clear
+   cloudflare-pointing
+   break
+   ;;
+   4 | 04)
+   clear
+   renewcert
+   break
+   ;;
+   5 | 05)
+   clear
+   banner
+   break
+   ;;
+   6 | 06)
+   clear
+   autoreboot
+   break
+   ;;
+   7 | 07)
+   clear
+   limit-speed
+   break
+   ;;
+   8 | 08)
+   clear
+   port-ssl
+   break
+   ;;
+   9 | 09)
+   clear
+   port-ovpn
+   break
+   ;;
+   10)
+   clear
+   port-wg
+   break
+   ;;
+   11)
+   clear
+   port-ws
+   break
+   ;;
+   12)
+   clear
+   port-tr
+   break
+   ;;
+   13)
+   clear
+   port-trgo
+   break
+   ;;
+   14)
+   clear
+   port-squid
+   break
+   ;;
+   15)
+   clear
+   port-sstp
+   break
+   ;;
+   16)
+   clear
+   port-dropbear
+   break
+   ;;
+   17)
+   clear
+   port-wssl
+   break
+   ;;
+   18)
+   clear
+   portohp
+   break
+   ;;
+   19)
+   clear
+   portxtls
+   break
+   ;;
+   20)
+   clear
+   port-vless
+   break
+   ;;
+   21)
+   clear
+   port-grpc
+   break
+   ;;
+   22)
+   clear
+   port-grws
+   break
+   ;;
+   23)
+   clear
+   port-grvl
+   break
+   ;;
+   24)
+   clear
+   port-grtr
+   break
+   ;;
+   100)
+   clear
+   restart-service
+   break
+   ;;
+   101)
+   clear
+   enabletorrent
+   break
+   ;;
+   102)
+   echo 3 > /proc/sys/vm/drop_caches && swapoff -a && swapon -a && printf '\n%s\n' 'Ram Cache Cleared'
+   setting-menu
+   break
+   ;;
+   111)
+   clear
+   kumbang
+   break
+   ;;
+   0 | 00)
+   clear
+   menu
+   break
+   ;;
+   *)
+   clear
+esac
+done
+#fim
