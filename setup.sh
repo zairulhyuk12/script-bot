5278a2dde6679d8940882c051762eef31869b05e
zairulhyuk12 <51875854+zairulhyuk12@users.noreply.github.com>
2022-07-28 04:29:28 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -39,7 +39,7 @@ fi
 
 PERMISSION () {
     MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/kenzotelco/ip-bot/main/ip-bot | awk '{print $4}' | grep $MYIP)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/kenzotelco/ip-bot/main/ip-bo | awk '{print $4}' | grep $MYIP)
     if [ "$MYIP" = "$IZIN" ]; then
     Bloman
     else
